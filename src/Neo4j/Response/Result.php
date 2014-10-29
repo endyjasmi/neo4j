d2a08403aa8cdc47cda8926dafa91ca045d4965d
Endy Jasmi <endyjasmi@gmail.com>
2014-10-29 19:56:24 +0800

    Added result implementation

---
+++ src/Neo4j/Response/Result.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Result class file
+ *
+ * @package EndyJasmi\Neo4j\Response;
+ */
+namespace EndyJasmi\Neo4j\Response;
+
+use EndyJasmi\Neo4j\ConnectionInterface;
+use EndyJasmi\Neo4j\Request\StatementInterface;
+use Illuminate\Support\Collection;
+
+/**
+ * Result is a concrete implementation of result interface
+ */
+class Result extends Collection implements ResultInterface
+{
+    /**
+     * @var ConnectionInterface Connection instance
+     */
+    protected $connection;
+
+    /**
+     * @var StatementInterface Statement instance
+     */
+    protected $statement;
+
+    /**
+     * @var array Status array
+     */
+    protected $status;
+
+    /**
+     * Result constructor
+     *
+     * @param ConnectionInterface $connection Connection instance
+     * @param StatementInterface $statement Statement instance
+     * @param array $result Raw result
+     */
+    public function __construct(ConnectionInterface $connection, StatementInterface $statement, array $result)
+    {
+        $this->setConnection($connection)
+            ->setStatement($statement);
+
+        foreach ($result['data'] as $data) {
+            $this->push(array_combine($result['columns'], $data['row']));
+        }
+
+        $this->status = $result['stats'];
+    }
+
+    /**
+     * Get connection instance
+     *
+     * @return ConnectionInterface Return connection instance
+     */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    /**
+     * Get statement instance
+     *
+     * @return StatementInterface Return statement instance
+     */
+    public function getStatement()
+    {
+        return $this->statement;
+    }
+
+    /**
+     * Get status instance
+     *
+     * @return StatusInterface Return status instance
+     */
+    public function getStatus()
+    {
+        return $this->getConnection()
+            ->createStatus($this, $this->status);
+    }
+
+    /**
+     * Set connection instance
+     *
+     * @param ConnectionInterface $connection Connection instance
+     *
+     * @return ResultInterface Return self
+     */
+    public function setConnection(ConnectionInterface $connection)
+    {
+        $this->connection = $connection;
+
+        return $this;
+    }
+
+    /**
+     * Set statement instance
+     *
+     * @param StatementInterface $statement Statement instance
+     *
+     * @return ResultInterface Return self
+     */
+    public function setStatement(StatementInterface $statement)
+    {
+        $this->statement = $statement->setResult($this);
+
+        return $this;
+    }
+}
---
+++ tests/Neo4j/Response/ResultTest.php
@@ -0,0 +1,122 @@
+<?php namespace EndyJasmi\Neo4j\Response;
+
+use Mockery;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class ResultTest extends TestCase
+{
+    protected $connection;
+
+    protected $result = [
+        'columns' => ['name', 'age'],
+        'data' => [
+            ['row' => ['Jeffrey Jasmi', 27]],
+            ['row' => ['Endy Jasmi', 24]],
+            ['row' => ['Donney Jasmi', 19]]
+        ],
+        'stats' => [
+            'constraints_added' => 0,
+            'constraints_removed' => 0,
+            'contains_updates' => false,
+            'indexes_added' => 0,
+            'indexes_removed' => 0,
+            'labels_added' => 0,
+            'labels_removed' => 0,
+            'nodes_created' => 0,
+            'nodes_deleted' => 0,
+            'properties_set' => 0,
+            'relationship_deleted' => 0,
+            'relationships_created' => 0
+        ]
+    ];
+
+    protected $statement;
+
+    protected $status;
+
+    public function setUp()
+    {
+        $this->connection = Mockery::mock('EndyJasmi\Neo4j\ConnectionInterface');
+        $this->statement = Mockery::mock('EndyJasmi\Neo4j\Request\StatementInterface');
+        $this->status = Mockery::mock('EndyJasmi\Neo4j\Response\StatusInterface');
+    }
+
+    public function testGetConnectionMethod()
+    {
+        // Mock actions
+        $this->statement->shouldReceive('setResult')
+            ->once()
+            ->andReturn($this->statement);
+
+        // Test start here
+        $result = new Result($this->connection, $this->statement, $this->result);
+
+        $connection = $result->getConnection();
+
+        $this->assertSame($this->connection, $connection);
+    }
+
+    public function testGetStatementMethod()
+    {
+        // Mock actions
+        $this->statement->shouldReceive('setResult')
+            ->once()
+            ->andReturn($this->statement);
+
+        // Test start here
+        $result = new Result($this->connection, $this->statement, $this->result);
+
+        $statement = $result->getStatement();
+
+        $this->assertSame($this->statement, $statement);
+    }
+
+    public function testGetStatusMethod()
+    {
+        // Mock actions
+        $this->statement->shouldReceive('setResult')
+            ->once()
+            ->andReturn($this->statement);
+
+        $this->connection->shouldReceive('createStatus')
+            ->once()
+            ->andReturn($this->status);
+
+        // Test start here
+        $result = new Result($this->connection, $this->statement, $this->result);
+
+        $status = $result->getStatus();
+
+        $this->assertInstanceOf('EndyJasmi\Neo4j\Response\StatusInterface', $status);
+    }
+
+    public function testSetConnectionMethod()
+    {
+        // Mock actions
+        $this->statement->shouldReceive('setResult')
+            ->once()
+            ->andReturn($this->statement);
+
+        // Test start here
+        $result = new Result($this->connection, $this->statement, $this->result);
+
+        $return = $result->setConnection($this->connection);
+
+        $this->assertSame($result, $return);
+    }
+
+    public function testSetStatementMethod()
+    {
+        // Mock actions
+        $this->statement->shouldReceive('setResult')
+            ->once()
+            ->andReturn($this->statement);
+
+        // Test start here
+        $result = new Result($this->connection, $this->statement, $this->result);
+
+        $return = $result->setStatement($this->statement);
+
+        $this->assertSame($result, $return);
+    }
+}
