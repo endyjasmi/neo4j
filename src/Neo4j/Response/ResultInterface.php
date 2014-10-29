d6c9136dde82c1939c95a78d4334f892548b9b2e
Endy Jasmi <endyjasmi@gmail.com>
2014-10-29 19:33:15 +0800

    Updated result interface

--- src/Neo4j/Response/ResultInterface.php
+++ src/Neo4j/Response/ResultInterface.php
@@ -11,53 +11,52 @@ use EndyJasmi\Neo4j\Request\StatementInterface;
 
 /**
  * ResultInterface is an interface for result class
- * @todo Extend collection
  */
 interface ResultInterface
 {
     /**
-     * Result constructor which auto set statement result
+     * Result constructor
      *
-     * @param ConnectionInterface $connection Result connection
-     * @param StatementInterface $statement Result statement
+     * @param ConnectionInterface $connection Connection instance
+     * @param StatementInterface $statement Statement instance
      * @param array $result Raw result
      */
     public function __construct(ConnectionInterface $connection, StatementInterface $statement, array $result);
 
     /**
-     * Get result connection
+     * Get connection instance
      *
-     * @return ConnectionInterface Return result connection
+     * @return ConnectionInterface Return connection instance
      */
     public function getConnection();
 
     /**
-     * Get result statement
+     * Get statement instance
      *
-     * @return StatementInterface Return result statement
+     * @return StatementInterface Return statement instance
      */
     public function getStatement();
 
     /**
-     * Get result status
+     * Get status instance
      *
-     * @return StatusInterface Return result status
+     * @return StatusInterface Return status instance
      */
     public function getStatus();
 
     /**
-     * Set result connection
+     * Set connection instance
      *
-     * @param ConnectionInterface $connection Result connection
+     * @param ConnectionInterface $connection Connection instance
      *
      * @return ResultInterface Return self
      */
     public function setConnection(ConnectionInterface $connection);
 
     /**
-     * Set result statement
+     * Set statement instance
      *
-     * @param StatementInterface $statement Result statement
+     * @param StatementInterface $statement Statement instance
      *
      * @return ResultInterface Return self
      */
