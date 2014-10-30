e3762ae137d111a1ade44512228b9fb73362dbdf
Endy Jasmi <endyjasmi@gmail.com>
2014-10-30 09:54:27 +0800

    Added stream driver implementation

---
+++ src/Neo4j/Driver/StreamDriver.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * StreamDriver class file
+ *
+ * @package EndyJasmi\Neo4j\Driver;
+ */
+namespace EndyJasmi\Neo4j\Driver;
+
+use Buzz\Client\FileGetContents as Client;
+use Buzz\Message\Request;
+use Buzz\Message\Response;
+use EndyJasmi\Neo4j\DriverInterface;
+use InvalidArgumentException;
+
+/**
+ * StreamDriver is a concrete implementation of driver interface
+ */
+class StreamDriver implements DriverInterface
+{
+    /**
+     * @var array Options array
+     */
+    protected $options = [];
+
+    /**
+     * Create host address string
+     *
+     * @return string Return address string
+     */
+    protected function createHost()
+    {
+        return "{$this->options['scheme']}://{$this->options['host']}:{$this->options['port']}";
+    }
+
+    /**
+     * Create request instance
+     *
+     * @param string $method Request method
+     * @param string $path Request path
+     * @param array $body Request body array
+     */
+    protected function createRequest($method, $path, array $body)
+    {
+        $request = new Request($method, $path, $this->createHost());
+
+        $request->setHeaders(
+            [
+                'Accept' => 'application/json; charset=UTF-8',
+                'Content-Type' => 'application/json',
+                'X-Stream' => 'true'
+            ]
+        );
+
+        $request->addHeader(
+            'Authorization: Basic ' .
+            base64_encode(
+                $this->options['username'] . ':' . $this->options['password']
+            )
+        );
+
+        $request->setContent(json_encode($body));
+
+        return $request;
+    }
+
+    /**
+     * Send request
+     *
+     * This method is used internally only, it is made public for partial mocking purposes
+     *
+     * @param Request $request Request instance
+     *
+     * @return array Return response array
+     */
+    public function send(Request $request)
+    {
+        $client = new Client;
+        $response = new Response;
+
+        $client->send($request, $response);
+
+        $location = $response->getHeader('location');
+        $response = json_decode($response->getContent(), true);
+
+        if (! is_null($location)) {
+            $location = explode('/', $location);
+            $id = array_pop($location);
+            $response['id'] = (int) $id;
+        }
+
+        return $response;
+    }
+
+    /**
+     * Driver constructor
+     *
+     * @param array $options Options array
+     */
+    public function __construct(array $options = [])
+    {
+        if (! empty($options)) {
+            $this->setOptions($options);
+        }
+    }
+
+    /**
+     * Begin a new transaction
+     *
+     * @param array $request Request array
+     *
+     * @return array Return Response array
+     */
+    public function beginTransaction(array $request)
+    {
+        $request = $this->createRequest('post', '/db/data/transaction', $request);
+
+        return $this->send($request);
+    }
+
+    /**
+     * Commit a transaction
+     *
+     * @param array $request Request array
+     *
+     * @return array Return response array
+     */
+    public function commitTransaction(array $request)
+    {
+        if (isset($request['id'])) {
+            $id = $request['id'];
+            unset($request['id']);
+
+            $path = "/db/data/transaction/$id/commit";
+        } else {
+            $path = '/db/data/transaction/commit';
+        }
+
+        $request = $this->createRequest('post', $path, $request);
+
+        return $this->send($request);
+    }
+
+    /**
+     * Execute an open transaction
+     *
+     * @param array $request Request array
+     *
+     * @return array Return response array
+     *
+     * @throws InvalidArgumentException If request does not have id
+     */
+    public function executeTransaction(array $request)
+    {
+        if (! array_key_exists('id', $request)) {
+            throw new InvalidArgumentException('Request does not have id');
+        }
+
+        $id = $request['id'];
+        unset($request['id']);
+
+        $request = $this->createRequest('post', "/db/data/transaction/$id", $request);
+
+        return $this->send($request);
+    }
+
+    /**
+     * Rollback an open transaction
+     *
+     * @param array $request Request array
+     *
+     * @return array Return response array
+     *
+     * @throws InvalidArgumentException If request does not have id
+     */
+    public function rollbackTransaction(array $request)
+    {
+        if (! array_key_exists('id', $request)) {
+            throw new InvalidArgumentException('Request does not have id');
+        }
+
+        $id = $request['id'];
+        unset($request['id']);
+
+        $request = $this->createRequest('delete', "/db/data/transaction/$id", $request);
+
+        return $this->send($request);
+    }
+
+    /**
+     * Set driver options
+     *
+     * @param array $options Options array
+     *
+     * @return DriverInterface Return self
+     */
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+}
---
+++ tests/Neo4j/Driver/StreamDriverTest.php
@@ -0,0 +1,143 @@
+<?php namespace EndyJasmi\Neo4j\Driver;
+
+use Mockery;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class StreamDriverTest extends TestCase
+{
+    protected $options = [
+        'scheme' => 'http',
+        'host' => 'localhost',
+        'port' => 7474,
+        'username' => '',
+        'password' => ''
+    ];
+
+    protected $request = [
+        'statements' => []
+    ];
+
+    protected $response = [
+        'results' => [],
+        'errors' => []
+    ];
+
+    public function testBeginTransactionMethod()
+    {
+        // Create partial mock
+        $client = Mockery::mock('EndyJasmi\Neo4j\Driver\StreamDriver[send]', [$this->options]);
+
+        // Mock actions
+        $client->shouldReceive('send')
+            ->once()
+            ->andReturn($this->response);
+
+        // Test start here
+        $response = $client->beginTransaction($this->request);
+
+        $this->assertArrayHasKey('results', $response);
+        $this->assertArrayHasKey('errors', $response);
+    }
+
+    public function testCommitTransactionMethod()
+    {
+        // Create partial mock
+        $client = Mockery::mock('EndyJasmi\Neo4j\Driver\StreamDriver[send]', [$this->options]);
+
+        // Mock actions
+        $client->shouldReceive('send')
+            ->once()
+            ->andReturn($this->response);
+
+        // Test start here
+        $response = $client->commitTransaction($this->request);
+
+        $this->assertArrayHasKey('results', $response);
+        $this->assertArrayHasKey('errors', $response);
+    }
+
+    public function testExecuteTransactionMethod()
+    {
+        // Create partial mock
+        $client = Mockery::mock('EndyJasmi\Neo4j\Driver\StreamDriver[send]', [$this->options]);
+
+        // Mock actions
+        $client->shouldReceive('send')
+            ->once()
+            ->andReturn($this->response);
+
+        // Test start here
+        $this->request['id'] = 1;
+        $response = $client->executeTransaction($this->request);
+
+        $this->assertArrayHasKey('results', $response);
+        $this->assertArrayHasKey('errors', $response);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testExecuteTransactionMethodThrowsInvalidArgumentException()
+    {
+        // Create partial mock
+        $client = Mockery::mock('EndyJasmi\Neo4j\Driver\StreamDriver[send]', [$this->options]);
+
+        // Mock actions
+        $client->shouldReceive('send')
+            ->once()
+            ->andReturn($this->response);
+
+        // Test start here
+        $response = $client->executeTransaction($this->request);
+
+        $this->assertArrayHasKey('results', $response);
+        $this->assertArrayHasKey('errors', $response);
+    }
+
+    public function testRollbackTransactionMethod()
+    {
+        // Create partial mock
+        $client = Mockery::mock('EndyJasmi\Neo4j\Driver\StreamDriver[send]', [$this->options]);
+
+        // Mock actions
+        $client->shouldReceive('send')
+            ->once()
+            ->andReturn($this->response);
+
+        // Test start here
+        $this->request['id'] = 1;
+        $response = $client->rollbackTransaction($this->request);
+
+        $this->assertArrayHasKey('results', $response);
+        $this->assertArrayHasKey('errors', $response);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testRollbackTransactionMethodThrowsInvalidArgumentException()
+    {
+        // Create partial mock
+        $client = Mockery::mock('EndyJasmi\Neo4j\Driver\StreamDriver[send]', [$this->options]);
+
+        // Mock actions
+        $client->shouldReceive('send')
+            ->once()
+            ->andReturn($this->response);
+
+        // Test start here
+        $response = $client->rollbackTransaction($this->request);
+
+        $this->assertArrayHasKey('results', $response);
+        $this->assertArrayHasKey('errors', $response);
+    }
+
+    public function testSetOptionsMethod()
+    {
+        $client = new StreamDriver($this->options);
+
+        $return = $client->setOptions($this->options);
+
+        $this->assertSame($client, $return);
+    }
+}
