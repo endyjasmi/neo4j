438d43516e7b70bebe0163ec806f87b9f0f3009a
Endy Jasmi <endyjasmi@gmail.com>
2014-10-30 08:43:39 +0800

    Fixed some syntax in errors test

--- tests/Neo4j/Response/ErrorsTest.php
+++ tests/Neo4j/Response/ErrorsTest.php
@@ -5,19 +5,19 @@ use PHPUnit_Framework_TestCase as TestCase;
 
 class ErrorsTest extends TestCase
 {
-    protected $errors = [
-        [
-            "code" => "Neo.ClientError.Statement.InvalidSyntax",
-            "message" => "Invalid input 'T': expected <init> (line 1, column 1)\n".
-                "'This is not a valid Cypher Statement.'\n ^"
-        ]
-    ];
-
     /**
      * @expectedException EndyJasmi\Neo4j\Error\Neo\ClientError\Statement\InvalidSyntax
      */
     public function testConstructorMethod()
     {
-        $errors = new Errors($this->errors);
+        $errorsArray = [
+            [
+                "code" => "Neo.ClientError.Statement.InvalidSyntax",
+                "message" => "Invalid input 'T': expected <init> (line 1, column 1)\n".
+                    "'This is not a valid Cypher Statement.'\n ^"
+            ]
+        ];
+
+        $errors = new Errors($errorsArray);
     }
 }
