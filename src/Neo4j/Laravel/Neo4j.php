8297b3b8d762dc978c8973b9ad04ba848ead0562
Endy Jasmi <endyjasmi@gmail.com>
2014-10-31 09:14:26 +0800

    Added laravel 5 basic class

---
+++ src/Neo4j/Laravel/Neo4j.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Neo4j class file
+ *
+ * @package EndyJasmi\Neo4j\Laravel;
+ */
+namespace EndyJasmi\Neo4j\Laravel;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Neo4j is a laravel facade
+ */
+class Neo4j extends Facade
+{
+    /**
+     * Get the registered name of the component
+     *
+     * @return string Return component name string
+     */
+    protected function getFacadeAccessor()
+    {
+        return 'neo4j';
+    }
+}
---
+++ src/Neo4j/Laravel/Neo4jServiceProvider.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Neo4jServiceProvider class file
+ *
+ * @package EndyJasmi\Neo4j\Laravel;
+ */
+namespace EndyJasmi\Neo4j\Laravel;
+
+use EndyJasmi\Neo4j;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Neo4jServiceProvider is a concrete implementation of service provider interface
+ */
+class Neo4jServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Run after every component is registered
+     *
+     * @param Dispatcher $event Event dispatcher instance
+     */
+    public function boot(Dispatcher $events)
+    {
+        AliasLoader::getInstance()
+            ->alias('Neo4j', 'EndyJasmi\Neo4j\Laravel\Neo4j');
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->app->bind('EndyJasmi\Neo4j\ConnectionInterface', 'EndyJasmi\Neo4j\Connection');
+        $this->app->bind('EndyJasmi\Neo4j\RequestInterface', 'EndyJasmi\Neo4j\Request');
+        $this->app->bind('EndyJasmi\Neo4j\Request\StatementInterface', 'EndyJasmi\Neo4j\Request\Statement');
+        $this->app->bind('EndyJasmi\Neo4j\ResponseInterface', 'EndyJasmi\Neo4j\Response');
+        $this->app->bind('EndyJasmi\Neo4j\Response\ErrorsInterface', 'EndyJasmi\Neo4j\Response\Errors');
+        $this->app->bind('EndyJasmi\Neo4j\Response\ResultInterface', 'EndyJasmi\Neo4j\Response\Result');
+        $this->app->bind('EndyJasmi\Neo4j\Response\StatusInterface', 'EndyJasmi\Neo4j\Response\Status');
+
+        $this->app->bindShared(
+            'neo4j',
+            function ($app) {
+                return new Neo4j($this->app);
+            }
+        );
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
